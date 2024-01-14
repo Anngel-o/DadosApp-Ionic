e6b0715960bb810d2a0786767278f75ca58ba319
Anngel-0 <anngelo.omz@gmail.com>
2024-01-13 19:33:02 -0600

    función tirar dados

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  dice = '';
+  img1 = '../../assets/img/dice3.png';
+  img2 = '../../assets/img/dice5.png';
 
   constructor() {}
 
+  launch(){
+    const numberDice1 = Math.floor(Math.random() * 6) + 1;
+    const numberDice2 = Math.floor(Math.random() * 6) + 1;
+
+    this.img1 = '../../assets/img/dice' + numberDice1 + '.png';
+    this.img2 = '../../assets/img/dice' + numberDice2 + '.png';
+  }
 }
